7485739164e9ad1f2a06c2b5ee17ccf783d0d59b
hayeon <linaad9809@gmail.com>
2024-01-05 13:41:31 +0900

    day5 commit

--- pathA/src/basic/day4/ProbStringReverse.java
+++ pathA/src/basic/day4/ProbStringReverse.java
@@ -3,7 +3,10 @@ package basic.day4;
 public class ProbStringReverse {
     public static void main(String[] args) {
         String msg = "Hello World";
+        
         reverseString(msg);
+
+        reverseString2(msg);
     }
 
     // 문자열을 뒤집는 메소드
@@ -14,4 +17,22 @@ public class ProbStringReverse {
             System.out.print(arr[i]);
         }
     }
+    // 문자열을 뒤집는 메소드2
+    private static void reverseString2(String msg) {
+        char[] arr = new char[msg.length()];
+        int idx = msg.length() - 1;
+
+        for(int i = 0 ; i < msg.length() ; i++) {
+            char temp = msg.charAt(i);
+            arr[idx] = temp;
+            // i=1일때 idx=9, i=2일때 idx=8 ...
+
+            idx--;     
+        }
+        
+        System.out.println("뒤집기 :");
+        System.out.println(arr);
+    }
+
+    
 }
