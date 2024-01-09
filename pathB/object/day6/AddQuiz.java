da0ad439e02945aeaba435e88f2f38d10e4b61e6
hejyu <linaad9809@gmail.com>
2024-01-10 00:13:30 +0900

    commit Quiz Generation Program

--- pathB/object/day6/AddQuiz.java
+++ pathB/object/day6/AddQuiz.java
@@ -7,28 +7,14 @@ public class AddQuiz {
     private int userAnswer;     // 제출한 숫자
 
     
-    public AddQuiz(int data1, int data2, int userAnswer) {
-        this.data1 = data1;
-        this.data2 = data2;
+    public AddQuiz(int start, int end) {    //매개변수가 2개인 커스텀 생성자를 생성
+        createQuiz(start, end);             //11~99사이의 덧셈 문제 생성
+    }
+    
+    public void setUserAnswer(int userAnswer) {
         this.userAnswer = userAnswer;
     }
-
-
-    /**
-     * 제출 숫자가 정답이면 참을 리턴하는 메소드
-     * @param num
-     * @return bol
-     */
-    public boolean isRight(int num){
-        boolean bol = false;
-
-        if (num == this.userAnswer) {
-            bol = true;
-        }
-        
-        return bol;
-    }
-
+    
     public int getData1() {
         return data1;
     }
@@ -42,7 +28,41 @@ public class AddQuiz {
     public int getUserAnswer() {
         return userAnswer;
     }
+    
 
+    public boolean checkAnswer(){
+        return (this.userAnswer == this.data1 + this.data2) ? true : false;
+    }
+
+    // 랜덤 숫자를 생성하는 함수
+    public int makeRandomNumber(int start, int end) {
+        return (int)(Math.random()*(end-start+1)) + start;
+    }
+
+    /**
+     * 시작값 마지막값 의 범위를 매개변수로 받아 
+     * AddQuiz 인스턴스 필드를 초기화하는 함수
+     * @param start
+     * @param end
+     */
+    public void createQuiz(int start, int end){     
+        this.data1 = makeRandomNumber(start, end);
+        this.data2 = makeRandomNumber(start, end);
+    }
+    
+    public String printResult() {
+        StringBuilder sb = new StringBuilder();
+        int answer;
+        char isCorrect;        
+
+        answer = this.data1 + this.data2;
+        isCorrect = checkAnswer() ? 'O' : 'X';
+        sb.append(String.format("문제 : %3d + %3d \t 제출한 답 : %3d \t 정답 : %3d \t 채점 : %c", this.data1, this.data2, this.userAnswer, answer, isCorrect));
+
+        return sb.toString();
+    }
+
+    
 
     
 }
--- pathB/object/day6/AddQuizMain.java
+++ pathB/object/day6/AddQuizMain.java
@@ -5,73 +5,42 @@ import java.util.Scanner;
 public class AddQuizMain {
 
     public static void main(String[] args) {
+        // 두 자리 랜덤 숫자 범위 셋팅
+        int start = 11;
+        int end = 99;
+        int correctCount = 0;                         //정답 개수
         
         // Scannere 사용, 문제 개수 입력받기 
         Scanner sc = new Scanner(System.in);
         
-        System.out.print("문제를 생성할 개수 입력 >>> ");
-        int i = 0;                      // 문제 번호
-        int temp = 0;
-        int num1 = 0;                   // 앞에 올 두자리 숫자
-        int num2 = 0;                   // 뒤에 올 두자리 숫자
-        String str = "";
+        // System.out.print("문제를 생성할 개수 입력 >>> ");
 
-        // 두 자리 랜덤 숫자 범위 셋팅
-        int start = 11;
-        int end = 99;
-        
         //문제 개수 입력받기 
-        int m_count = sc.nextInt();     // 풀 문제 수
-        
+        // int m_count = sc.nextInt();     // 풀 문제 수
+        int m_count = 2;
         if(m_count == 0) {
             System.out.println("프로그램을 종료합니다");
             return;
         }
 
-        String[] quizArr = new String[m_count];     //문제를 저장할 배열을 생성 
-        int[] userArr = new int[m_count];           //학생 입력 답 저장한 배열을 생성    
-        int[] answerArr = new int[m_count];         //문제 정답 배열 생성
-        char[] markArr   = new char[m_count];       //채점 배열 생성
-        int rightCount = 0;                         //맞은 개수
-        
-
         System.out.println(String.format("덧셈. %2d개 문제 퀴즈 시작", m_count));
-        AddQuiz[] quizs = new AddQuiz[m_count];     // 클래스 배열 
-
+        AddQuiz[] quizs = new AddQuiz[m_count];     // 객체 배열 초기화
+        
         try {
-            while (i < m_count) {
-                str = "";
+            int i = 0;
+            while (true) {
+                if(i > (m_count-1)) break;
                 
-                //문제 생성 
-                for(int j = 0; j < 2 ; j++){
-                    temp = getRandomNumber(start, end);
-                    
-                    if(j == 1) {
-                        str += "+";
-                        num2 = temp;
-                    }else {
-                        num1 = temp;
-                    }
-                    
-                    str += temp;        
-                }
+                quizs[i] = new AddQuiz(start, end);
                 
-                int answer = sum(num1, num2);
-                answerArr[i] = answer;          //정답 배열 인덱스에 답 저장
-                quizArr[i] = str + " = ";       //문제 배열 인덱스에 문제 저장
-                
-                System.out.print(String.format("%s", quizArr[i]));
+                System.out.println(String.format("퀴즈 %d번.%3d +%3d ", i+1, quizs[i].getData1(), quizs[i].getData2()));
                 System.out.print("답을 입력하세요>>> ");
                 
                 int userAnswer = sc.nextInt();           // 학생의 답 입력받기  
-                userArr[i] = userAnswer;
-                
-                quizs[i] = new AddQuiz(num1, num2, userAnswer);     // 커스텀 생성자 사용
-                boolean bol = quizs[i].isRight(answer);
+                quizs[i].setUserAnswer(userAnswer);      // 문제에 입력한 답을 저장
+                boolean bol = quizs[i].checkAnswer();    // 정답인지 체크, 정답이면 true, 오답이면 false 를 리턴
 
-                if(bol)  markArr[i] = 'O';
-                else     markArr[i] = 'X';
-
+                if(bol)     correctCount += 1;           // 정답이면 correcCount 1 증가
 
                 i += 1;
             }
@@ -79,29 +48,15 @@ public class AddQuizMain {
             System.out.println(e);
         }
         
-
         System.out.println("::: 채점하고 있습니다:::");
-        System.out.println(String.format("%20s\t %10s\t %10s\t %1s", "문제", "제출한 답", "정답", "채점"));
-        
-        for(int m = 0; m < m_count ; m++){
-            System.out.println(String.format("%2d번. \t %14s\t %10d\t %10d\t %c\t", m, quizArr[m].substring(0, 5), userArr[m], answerArr[m], markArr[m]));
-            
-            if (markArr[m] == 'O') rightCount += 1;
+        for(int i=0 ; i < quizs.length ; i++ ) {
+            System.out.println(quizs[i].printResult());
         }
 
-        System.out.println(String.format("::: 맞은 개수는 %d개 입니다:::", rightCount));
+        System.out.println(String.format("::: 정답 개수는 %d개 입니다:::", correctCount));
     }
     
 
-    public static int getRandomNumber(int start, int end) {
-        int temp = (int)(Math.random()*(end-start+1)) + start;
-
-        return temp;
-    }
-
-    public static int sum(int num1, int num2) {
-        return num1+num2;
-    }
-
+    
 
 }
